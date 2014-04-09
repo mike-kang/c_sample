6328e88202e04317b23c0ed547e8d71e274bbdbe
ksflex1 <ksflex@naver.com>
2014-04-10 06:24:34 +0900

    get cpu core count

---
+++ readcpu.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main( )
+{
+    FILE *pFile = NULL;
+ 
+    pFile = fopen("/proc/cpuinfo", "r");
+    if( pFile != NULL )
+    {
+        char strTemp[255];
+        char *pStr;
+		int count = 0;
+ 
+        while( !feof( pFile ) )
+        {
+            pStr = fgets( strTemp, sizeof(strTemp), pFile );
+            //printf( "%s", pStr );
+			if (pStr) {
+				if(strstr(pStr, "processor"))
+					count++;
+			} 	    
+        }
+		printf("count = %d\n", count);
+        fclose( pFile );
+    }
+    else
+    {
+        //에러 처리
+    }
+ 
+    return 0;
+}
